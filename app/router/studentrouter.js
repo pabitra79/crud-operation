c0ac9083344efcd1a4330d6907d856830ff4838b
barik.pabitra <barikpabitra79@gmail.com>
2025-08-22 23:57:39 +0530

    route change

--- app/router/studentrouter.js
+++ app/router/studentrouter.js
@@ -3,7 +3,7 @@ const studentController = require("../controller/studentcontroller");
 const router = express.Router();
 
 router.post("/create-student", studentController.createStudent);
-router.get("/get-student", studentController.getsStudent);
+router.get("/", studentController.getsStudent);
 router.get("/add-student", studentController.addStduent);
 router.post("/update/students/:id", studentController.updateStudent);
 router.get("/edit/students/:id", studentController.editStudent);
