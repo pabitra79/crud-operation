eb12f0b43bac470ac19595f8fcda3734679258d2
barik.pabitra <barikpabitra79@gmail.com>
2025-08-23 12:24:17 +0530

    update at app

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set("views", "views");
 
 app.use(express.static("public"));
 
-app.use(studentrouter);
+app.use("/", studentrouter);
 
 app.listen(3001, () => {
   console.log("server port 3001 ");
